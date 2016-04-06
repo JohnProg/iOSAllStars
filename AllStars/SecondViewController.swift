d8dd6d00d285cdb7bcde9a1d3204091ff049596f
Rodrigo Gonzalez <rgonzalez@belatrixsf.com>
2016-04-06 16:07:43 -0500

    initial commit of the project

---
+++ AllStars/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AllStars
+//
+//  Created by Rodrigo Gonzalez on 4/6/16.
+//  Copyright © 2016 Belatrix. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
