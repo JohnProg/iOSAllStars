2422976850cd0f15ac5ab8efb509c1d14a8e1ead
Rodrigo Gonzalez <rgonzalez@belatrixsf.com>
2016-04-07 11:02:51 -0500

    added LoginViewController created Utils

---
+++ AllStars/Extra/Utils.swift
@@ -0,0 +1,14 @@
+//
+//  Utils.swift
+//  AllStars
+//
+//  Created by Rodrigo Gonzalez on 4/7/16.
+//  Copyright © 2016 Belatrix. All rights reserved.
+//
+
+import UIKit
+
+class Utils: NSObject {
+
+    static let mainColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0)
+}
---
+++ AllStars/Login/ViewController/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  AllStars
+//
+//  Created by Rodrigo Gonzalez on 4/7/16.
+//  Copyright © 2016 Belatrix. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Login"
+        view.backgroundColor = Utils.mainColor
+    }
+}
